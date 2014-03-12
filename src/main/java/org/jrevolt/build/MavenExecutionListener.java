b4802717974b1bee519e7e149b903c53eeb2b83e
Patrik Beno <patrikbeno@gmail.com>
2014-03-12 19:48:44 +0100

    minor: code formatting

--- src/main/java/org/jrevolt/build/MavenExecutionListener.java
+++ src/main/java/org/jrevolt/build/MavenExecutionListener.java
@@ -18,169 +18,169 @@ import java.util.Map;
  */
 public class MavenExecutionListener extends AbstractExecutionListener {
 
-    static Map<MavenSession, MavenExecutionListener> listenerBySession = new HashMap<MavenSession, MavenExecutionListener>();
+	static Map<MavenSession, MavenExecutionListener> listenerBySession = new HashMap<MavenSession, MavenExecutionListener>();
 
-    static public synchronized MavenExecutionListener forSession(MavenSession session) {
-        MavenExecutionListener listener = listenerBySession.get(session);
-        if (listener == null) {
-            listener = new MavenExecutionListener(session);
-            listener.install();
-            listenerBySession.put(session, listener);
-        }
-        return listener;
-    }
+	static public synchronized MavenExecutionListener forSession(MavenSession session) {
+		MavenExecutionListener listener = listenerBySession.get(session);
+		if (listener == null) {
+			listener = new MavenExecutionListener(session);
+			listener.install();
+			listenerBySession.put(session, listener);
+		}
+		return listener;
+	}
 
-    MavenSession session;
-    ExecutionListener original;
-    List<ExecutionListener> listeners;
+	MavenSession session;
+	ExecutionListener original;
+	List<ExecutionListener> listeners;
 
-    private MavenExecutionListener(MavenSession session) {
-        this.session = session;
-        this.listeners = synchronizedList(new LinkedList<ExecutionListener>());
-    }
+	private MavenExecutionListener(MavenSession session) {
+		this.session = session;
+		this.listeners = synchronizedList(new LinkedList<ExecutionListener>());
+	}
 
-    ///
+	///
 
-    private synchronized void install() {
-        original = session.getRequest().getExecutionListener();
-        addListener(original);
-        session.getRequest().setExecutionListener(this);
-    }
+	private synchronized void install() {
+		original = session.getRequest().getExecutionListener();
+		addListener(original);
+		session.getRequest().setExecutionListener(this);
+	}
 
-    private synchronized void uninstall() {
-        removeListener(original);
-        session.getRequest().setExecutionListener(original);
-    }
+	private synchronized void uninstall() {
+		removeListener(original);
+		session.getRequest().setExecutionListener(original);
+	}
 
-    public void addListener(ExecutionListener listener) {
-        listeners.add(listener);
-    }
+	public void addListener(ExecutionListener listener) {
+		listeners.add(listener);
+	}
 
-    public void removeListener(ExecutionListener listener) {
-        listeners.remove(listener);
-    }
+	public void removeListener(ExecutionListener listener) {
+		listeners.remove(listener);
+	}
 
-    ///
+	///
 
 
-    @Override
-    public void forkedProjectFailed(ExecutionEvent event) {
-        for (ExecutionListener listener : listeners) {
-            listener.forkedProjectFailed(event);
-        }
-    }
+	@Override
+	public void forkedProjectFailed(ExecutionEvent event) {
+		for (ExecutionListener listener : listeners) {
+			listener.forkedProjectFailed(event);
+		}
+	}
 
-    @Override
-    public void forkedProjectSucceeded(ExecutionEvent event) {
-        for (ExecutionListener listener : listeners) {
-            listener.forkedProjectSucceeded(event);
-        }
-    }
+	@Override
+	public void forkedProjectSucceeded(ExecutionEvent event) {
+		for (ExecutionListener listener : listeners) {
+			listener.forkedProjectSucceeded(event);
+		}
+	}
 
-    @Override
-    public void forkedProjectStarted(ExecutionEvent event) {
-        for (ExecutionListener listener : listeners) {
-            listener.forkedProjectStarted(event);
-        }
-    }
+	@Override
+	public void forkedProjectStarted(ExecutionEvent event) {
+		for (ExecutionListener listener : listeners) {
+			listener.forkedProjectStarted(event);
+		}
+	}
 
-    @Override
-    public void mojoFailed(ExecutionEvent event) {
-        for (ExecutionListener listener : listeners) {
-            listener.mojoFailed(event);
-        }
-    }
+	@Override
+	public void mojoFailed(ExecutionEvent event) {
+		for (ExecutionListener listener : listeners) {
+			listener.mojoFailed(event);
+		}
+	}
 
-    @Override
-    public void mojoSucceeded(ExecutionEvent event) {
-        for (ExecutionListener listener : listeners) {
-            listener.mojoSucceeded(event);
-        }
-    }
+	@Override
+	public void mojoSucceeded(ExecutionEvent event) {
+		for (ExecutionListener listener : listeners) {
+			listener.mojoSucceeded(event);
+		}
+	}
 
-    @Override
-    public void mojoStarted(ExecutionEvent event) {
-        for (ExecutionListener listener : listeners) {
-            listener.mojoStarted(event);
-        }
-    }
+	@Override
+	public void mojoStarted(ExecutionEvent event) {
+		for (ExecutionListener listener : listeners) {
+			listener.mojoStarted(event);
+		}
+	}
 
-    @Override
-    public void mojoSkipped(ExecutionEvent event) {
-        for (ExecutionListener listener : listeners) {
-            listener.mojoSkipped(event);
-        }
-    }
+	@Override
+	public void mojoSkipped(ExecutionEvent event) {
+		for (ExecutionListener listener : listeners) {
+			listener.mojoSkipped(event);
+		}
+	}
 
-    @Override
-    public void forkFailed(ExecutionEvent event) {
-        for (ExecutionListener listener : listeners) {
-            listener.forkFailed(event);
-        }
-    }
+	@Override
+	public void forkFailed(ExecutionEvent event) {
+		for (ExecutionListener listener : listeners) {
+			listener.forkFailed(event);
+		}
+	}
 
-    @Override
-    public void forkSucceeded(ExecutionEvent event) {
-        for (ExecutionListener listener : listeners) {
-            listener.forkSucceeded(event);
-        }
-    }
+	@Override
+	public void forkSucceeded(ExecutionEvent event) {
+		for (ExecutionListener listener : listeners) {
+			listener.forkSucceeded(event);
+		}
+	}
 
-    @Override
-    public void forkStarted(ExecutionEvent event) {
-        for (ExecutionListener listener : listeners) {
-            listener.forkStarted(event);
-        }
-    }
+	@Override
+	public void forkStarted(ExecutionEvent event) {
+		for (ExecutionListener listener : listeners) {
+			listener.forkStarted(event);
+		}
+	}
 
-    @Override
-    public void projectFailed(ExecutionEvent event) {
-        for (ExecutionListener listener : listeners) {
-            listener.projectFailed(event);
-        }
-    }
+	@Override
+	public void projectFailed(ExecutionEvent event) {
+		for (ExecutionListener listener : listeners) {
+			listener.projectFailed(event);
+		}
+	}
 
-    @Override
-    public void projectSucceeded(ExecutionEvent event) {
-        for (ExecutionListener listener : listeners) {
-            listener.projectSucceeded(event);
-        }
-    }
+	@Override
+	public void projectSucceeded(ExecutionEvent event) {
+		for (ExecutionListener listener : listeners) {
+			listener.projectSucceeded(event);
+		}
+	}
 
-    @Override
-    public void projectStarted(ExecutionEvent event) {
-        for (ExecutionListener listener : listeners) {
-            listener.projectStarted(event);
-        }
-    }
+	@Override
+	public void projectStarted(ExecutionEvent event) {
+		for (ExecutionListener listener : listeners) {
+			listener.projectStarted(event);
+		}
+	}
 
-    @Override
-    public void projectSkipped(ExecutionEvent event) {
-        for (ExecutionListener listener : listeners) {
-            listener.projectSkipped(event);
-        }
-    }
+	@Override
+	public void projectSkipped(ExecutionEvent event) {
+		for (ExecutionListener listener : listeners) {
+			listener.projectSkipped(event);
+		}
+	}
 
-    @Override
-    public void sessionEnded(ExecutionEvent event) {
-        for (ExecutionListener listener : listeners) {
-            listener.sessionEnded(event);
-        }
-        listenerBySession.remove(event.getSession());
-        uninstall();
-    }
+	@Override
+	public void sessionEnded(ExecutionEvent event) {
+		for (ExecutionListener listener : listeners) {
+			listener.sessionEnded(event);
+		}
+		listenerBySession.remove(event.getSession());
+		uninstall();
+	}
 
-    @Override
-    public void sessionStarted(ExecutionEvent event) {
-        for (ExecutionListener listener : listeners) {
-            listener.sessionStarted(event);
-        }
-    }
+	@Override
+	public void sessionStarted(ExecutionEvent event) {
+		for (ExecutionListener listener : listeners) {
+			listener.sessionStarted(event);
+		}
+	}
 
-    @Override
-    public void projectDiscoveryStarted(ExecutionEvent event) {
-        for (ExecutionListener listener : listeners) {
-            listener.projectDiscoveryStarted(event);
-        }
-    }
+	@Override
+	public void projectDiscoveryStarted(ExecutionEvent event) {
+		for (ExecutionListener listener : listeners) {
+			listener.projectDiscoveryStarted(event);
+		}
+	}
 }
